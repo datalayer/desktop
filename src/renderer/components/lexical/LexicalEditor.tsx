3a35fcaf04e5b02e94c09bc51a929bdd7440badb
Eric Charles <eric@datalayer.io>
2026-02-21 19:51:53 +0100

    fix: runtime for lexical

--- src/renderer/components/lexical/LexicalEditor.tsx
+++ src/renderer/components/lexical/LexicalEditor.tsx
@@ -341,6 +341,7 @@ export function LexicalEditor({
                   <LexicalEditorContainer
                     collaboration={collaboration}
                     onContentChange={onContentChange}
+                    serviceManager={serviceManager}
                   />
                 </div>
               </ToolbarContext>
